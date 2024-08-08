1d50ab222d91174c7b1133e9784db1e5764e3f54
aysegulsum <aysegulsumeren@gmail.com>
2024-08-08 13:35:11 +0300

    login messages work correctly

--- src/App.js
+++ src/App.js
@@ -2,57 +2,22 @@ import React, { useEffect, useState } from "react";
 import Appbar from "./Appbar";
 import Textfields from "./TextField";
 
-/*function WelcomeMessage() {
-    const [message, setMessage] = useState('');
-
-    useEffect(() => {
-        fetch('http://localhost:8080/welcome')
-            .then(response => response.text())
-            .then(data => setMessage(data))
-            .catch(error => console.error('Error fetching message:', error));
-    }, []);
-
-    return (
-        <div>
-          <AppBar/>
-            <h1>Hoş Geldiniz!</h1>
-            <p>{message}</p>
-        </div>
-    );
-}
-
-export default WelcomeMessage;*/
-
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/welcome")
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("Error fetching message:", error));
+  }, []);
+
   return (
     <div classname="App">
       <Appbar />
       <Textfields />
+      <p align="center">{message}</p>
     </div>
   );
 }
 export default App;
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
--- src/TextField.js
+++ src/TextField.js
@@ -1,94 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Button, Paper, Container } from "@mui/material/";
-//import Icon from "react-native-vector-icons/FontAwesome";
+import { Button, Paper, Container, Typography } from "@mui/material/";
 
 export default function Textfields() {
-  const [username, setName] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-  const handleClick = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    const textField = { username, password };
-    console.log(textField);
+    setMessage(""); // Clear the message state
+    try {
+      const response = await fetch("http://localhost:8080/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+
+      const messageFromServer = await response.text();
+      setMessage(messageFromServer);
+    } catch (error) {
+      setMessage("An error occurred. Please try again.");
+    }
   };
-  // <Icon name="users" size={20} />
+
   return (
     <Container component="main" maxWidth="xs">
       <Paper elevation={3} sx={{ padding: 2, margin: 2 }}>
-        <h2 align="center">LOGIN</h2>
+        <Typography variant="h5" align="center">
+          LOGIN
+        </Typography>
+
         <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <TextField
-            id="outlined-basic"
-            label="Name"
-            variant="outlined"
-            value={username}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="success"
-            onClick={handleClick}
-            sx={{ backgroundColor: "purple", color: "white" }}
-          >
-            Login
-          </Button>
+          <form onSubmit={handleLogin}>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="success"
+              sx={{ backgroundColor: "purple", color: "white" }}
+            >
+              Login
+            </Button>
+          </form>
+          {message && (
+            <Typography color="textSecondary" variant="body2" align="center">
+              {message}
+            </Typography>
+          )}
         </Box>
       </Paper>
     </Container>
   );
 }
-
-/*
-export default function Textfields() {
-  // const paperStyle={padding:'50px 20px', width:600, margin:"20px auto"}
-  const [username, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    const textField = { username, password };
-    console.log(textField);
-  };
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <h2 align="center">LOGIN</h2>
-      <TextField
-        id="outlined-basic"
-        label="Name"
-        variant="outlined"
-        value={username}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <TextField
-        id="outlined-basic"
-        label="Password"
-        variant="outlined"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        color="success"
-        onClick={handleClick}
-      >
-        Login
-      </Button>
-    </Container>
-  );
-}
-*/
