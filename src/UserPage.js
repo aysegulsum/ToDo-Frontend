10d65017b6c56af23802d02c01cffc8cb2177643
aysegulsum <aysegulsumeren@gmail.com>
2024-09-03 12:47:03 +0300

    clean up

--- src/UserPage.js
+++ src/UserPage.js
@@ -122,7 +122,7 @@ const UserPage = () => {
             .then((response) => response.text())
             .then((data) => {
                 console.log("Success:", data);
-                //window.location.reload();
+
             })
             .catch((error) => {
                 console.error("Error:", error);
